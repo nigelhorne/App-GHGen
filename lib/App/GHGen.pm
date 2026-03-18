1eae01b400a36c391ee03ddea1f2f941cc320d48
Nigel Horne <njh@bandsman.co.uk>
2026-03-18 11:56:05 -0400

    Use v4

--- lib/App/GHGen.pm
+++ lib/App/GHGen.pm
@@ -1,7 +1,7 @@
 package App::GHGen;
 
 # Try this to update GitHub Marketplace released, but bear in mind it tends to not work, marketplace does not detect the update
-# Update the @v1 to @v2 in actions.yml
+# Update the @v1 to @v2 in action.yml
 # git tag -a v2 -m 'Release v0.02'
 # git push origin v2
 # Go to https://github.com/nigelhorne/App-GHGen/releases
